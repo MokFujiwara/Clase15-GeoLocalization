9b9a5a241d6950cfdb8d8508c44c540c16541bf0
MokFujiwara <felipe7550@gmail.com>
2021-10-13 13:11:38 -0300

    pit confirm

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+/**importar librerías */
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { LoadingController } from '@ionic/angular';
+declare var google;
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  //coordenadas: (lat:53.46424017781441, lng:-2.291142394046567)
+  lat: number = 53.46424017781441;
+  lng: number = -2.291142394046567;
+  constructor(
+    private geoLoca: Geolocation,
+    private loadingCtrl: LoadingController) { }
 
-  constructor() {}
+  ngOnInit() {
+  this.cargarMapa();
+  }
 
+  async cargarMapa() {
+    const cargar = await this.loadingCtrl.create({
+      message: "Cargando mapa..."
+    });
+    await cargar.present();
+    const ubicacion = {
+      lat: this.lat,
+      lng: this.lng
+    };
+    const mapaHtml: HTMLElement = document.getElementById("map");
+    const map = new google.maps.Map(mapaHtml, {
+      center: ubicacion,
+      zoom: 12
+    });
+    google.maps.event.addListenerOnce(map, 'idle', () => {
+      cargar.dismiss();
+    });
+  }
 }
